7c3fa9fdc0e8694f67fea3d62c93d165a23feca4
Akshata-Projects <44542760+Akshata-Projects@users.noreply.github.com>
2022-02-17 17:48:56 +0400

    Initialized MERN Project

---
+++ backend/data/notes.js
@@ -0,0 +1,26 @@
+const notes = [
+  {
+    _id: "1",
+    title: "1st data",
+    content: "This is a dummy text",
+  },
+  {
+    _id: "2",
+    title: "2nd data",
+    content: "This is a dummy text",
+  },
+
+  {
+    _id: "3",
+    title: "3rd data",
+    content: "This is a dummy text",
+  },
+
+  {
+    _id: "4",
+    title: "4th data",
+    content: "This is a dummy text",
+  },
+];
+
+module.exports = notes;
